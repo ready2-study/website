8bd9c13f040f4d663fa727de8715eefa8fab7b6f
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-02-20 13:31:14 +0100

    feat(navbar): add full, working and scalable navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,35 @@
 import Link from "next/link";
 import React from "react";
 
-export default function Navbar() {
+const navNames: string[] = ["Questions", "Tutoring", "About"];
+
+const NavigationLink = ({ linkName }: { linkName: string }) => {
   return (
-    <div className="font-bold py-6">
+    <Link className="text-sm mx-5" href={`/${linkName}`}>
+      {linkName}
+    </Link>
+  );
+};
+
+const Navigation = () => {
+  return (
+    <div className="flex items-center">
       <Link className="flex text-3xl" href="/">
         Ready2<div className="text-additional">.Study</div>
       </Link>
+      <div className="flex mx-5">
+        {navNames.map((name: string) => (
+          <NavigationLink key={name} linkName={name} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default function Navbar() {
+  return (
+    <div className="font-bold py-6">
+      <Navigation />
     </div>
   );
 }
