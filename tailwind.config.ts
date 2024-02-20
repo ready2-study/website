f9058f5efa6df5380c2bb6ae7935d452cde4e171
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-02-20 12:52:00 +0100

    feat(tailwind): add custom config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,29 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
+    colors: {
+      primary: {
+        DEFAULT: "#353535",
+        dark: "#272727",
+      },
+      secondary: {
+        DEFAULT: "#ffffff",
+        dark: "#e8e8e8",
+      },
+      additional: {
+        DEFAULT: "#892DFF",
+        dark: "#6E20D1",
+      },
+    },
     container: {
       center: true,
       padding: "2rem",
@@ -18,6 +32,9 @@ const config = {
       },
     },
     extend: {
+      spacing: {
+        box: "1180px",
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -35,6 +52,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
