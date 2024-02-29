0c3f067c72de03d6f76dcfe25b2e60d4d084ad42
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-02-29 16:21:48 +0100

    feat(footer): implement responsive design for a footer

--- src/components/additional/footer/Footer.tsx
+++ src/components/additional/footer/Footer.tsx
@@ -4,11 +4,11 @@ import LanguagesFooter from "./LanguagesFooter";
 export default function Footer() {
   return (
     <div className="bg-black flex items-center justify-center font-bold">
-      <div className="w-box">
-        <div className="flex text-2xl pt-[28px]">
+      <div className=" lg:w-boxTablet xl:w-box">
+        <div className="flex items-center lg:items-start justify-center lg:justify-start text-3xl lg:text-2xl pt-[28px]">
           Ready2<div className="text-additional">.Study</div>
         </div>
-        <div className="flex text-lg flex-row py-5">
+        <div className="hidden lg:flex text-lg flex-row py-5">
           <div className="flex flex-col  pr-[200px]">
             <h1 className="text-additional text-xl py-2">Navigation</h1>
             <Link
@@ -52,15 +52,15 @@ export default function Footer() {
             </Link>
           </div>
         </div>
-        <div className="pt-5 pb-10 flex items-center justify-between">
-          <div className="font-light text-sm leading-4 text-secondary-dark">
+        <div className="pt-5 pb-10 flex flex-col-reverse lg:flex-row items-center justify-between">
+          <div className="text-center p-5 lg:p-0 lg:text-left font-light text-sm leading-4 text-secondary-dark">
             Ready2.study is not affiliated to or endorsed by any school, college
             or university
             <br />
             <br />
             Copyright © 2024
           </div>
-          <div>
+          <div className="p-5 lg:p-0">
             <LanguagesFooter />
           </div>
         </div>
