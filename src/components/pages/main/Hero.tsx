ff31fc52a7f062a7540ab589ffded7272a1de615
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-02-29 14:38:00 +0100

    feat(hero): implement responsive design for hero section of main page

--- src/components/pages/main/Hero.tsx
+++ src/components/pages/main/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <div className="flex items-center justify-center">
-      <div className="h-[876px] w-box flex items-center justify-center">
+      <div className="h-[876px] w-boxPhone md:w-boxTablet lg:w-box flex items-center justify-center">
         <div className="flex flex-col items-center justify-center">
           <Image
             src="/Background.png"
@@ -14,22 +14,18 @@ export default function Hero() {
             alt="bg"
             className="pointer-events-none absolute top-0 left-0  w-full h-[1010px] -z-20"
           />
-          <h1 className="font-bold text-7xl max-w-[900px] text-center inline-block">
+          <h1 className="font-bold text-6xl lg:text-7xl max-w-[900px] text-center inline-block">
             Study for your exams{" "}
             <p className="text-additional inline">directly</p> from the source
           </h1>
 
-          <div className="text-center m-10 text-container">
-            <p className="font-normal text-lg">
-              We are the largest database of university exams questions.
-            </p>
-            <p className="font-normal text-lg">
-              Find your college and start learning!
-            </p>
+          <div className="font-normal text-base lg:text-lg text-center m-8 xl:m-10 text-container">
+            <p>We are the largest database of university exams questions.</p>
+            <p>Find your college and start learning!</p>
           </div>
 
           <Input
-            className="max-w-[900px] h-[60px] text-black"
+            className="max-w-[900px] h-[55px] xl:h-[60px] text-black"
             type="Search for university, question or subject that you are interested in..."
             placeholder="Search for university, question or subject that you are interested in..."
           />
